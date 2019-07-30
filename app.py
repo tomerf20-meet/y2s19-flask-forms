c8b7cba1142332590b4fcbef0691a8f7d64f8a6e
myname21-meet <myname21@meet.mit.edu>
2019-07-30 11:32:46 -0400

    flask labs

--- app.py
+++ app.py
@@ -10,7 +10,25 @@ def home():
 def display_student(student_id):
     return render_template('student.html', student=query_by_id(student_id))
 
-#Create an '/add' route here:
+@app.route("/add", methods=["GET", "POST"])
+def add_student_route():
+	if request.method == 'GET':
+		return render_template('add.html')
+	else:
+		student_name=request.form["student_name"]
+		student_year=request.form["student_year"]
+		student_finished_lab=request.form["student_finished_lab"]
+		student_finished_lab=student_finished_lab=="True"
+		add_student(student_name, student_year, student_finished_lab)
+		print("Received POST request!")
+		return render_template("add.html")
+
+@app.route("/delete/<int:student_id>", methods=["post"])
+def delete(student_id):
+	delete_student_id(student_id)
+	return render_template('home.html', students=query_all())
+
 
 if __name__ == '__main__':
     app.run(debug=True)
+
